9aa6f7c69fa6fe4e8cc80a7fe75fbdcb897c0bf8
Alex Gelman <60175171+alexg-cybr@users.noreply.github.com>
2021-07-13 10:38:34 +0300

    Provider private subnet bug example

--- bin/cdk-cr-bug.ts
+++ bin/cdk-cr-bug.ts
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkCrBugStack } from '../lib/cdk-cr-bug-stack';
+import { MyCustomResource } from '../lib/cdk-cr-bug-stack';
+
+/**
+ * A stack that sets up MyCustomResource and shows how to get an attribute from it
+ */
+ class MyStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new MyCustomResource(this, 'DemoResource', {
+      message: 'CustomResource says hello',
+    });
+  }
+}
 
 const app = new cdk.App();
-new CdkCrBugStack(app, 'CdkCrBugStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new MyStack(app, 'CustomResourceDemoStack', { 
+  env: { 
+    account: process.env.CDK_DEFAULT_ACCOUNT, 
+    region: process.env.CDK_DEFAULT_REGION 
+}});
+app.synth();
--- lib/cdk-cr-bug-stack.ts
+++ lib/cdk-cr-bug-stack.ts
@@ -1,9 +1,74 @@
-import * as cdk from '@aws-cdk/core';
+import cdk = require('@aws-cdk/core');
+import iam = require('@aws-cdk/aws-iam');
+import ec2 = require('@aws-cdk/aws-ec2')
+import lambda = require('@aws-cdk/aws-lambda');
+import { PythonFunction } from "@aws-cdk/aws-lambda-python";
+import logs = require('@aws-cdk/aws-logs')
+import cr = require('@aws-cdk/custom-resources');
 
-export class CdkCrBugStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+import fs = require('fs');
 
-    // The code that defines your stack goes here
+export interface MyCustomResourceProps {
+  /**
+   * Message to echo
+   */
+  message: string;
+}
+
+export class MyCustomResource extends cdk.Construct {
+  public readonly response: string;
+
+  constructor(scope: cdk.Construct, id: string, props: MyCustomResourceProps) {
+    super(scope, id);
+
+    // const resource = new cfn.CustomResource(this, 'Resource', {
+    //   provider: cfn.CustomResourceProvider.lambda(new lambda.SingletonFunction(this, 'Singleton', {
+    //     uuid: 'f7d4f730-4ee1-11e8-9c2d-fa7ae01bbebc',
+    //     code: new lambda.InlineCode(fs.readFileSync('lib/custom-resource-handler.py', { encoding: 'utf-8' })),
+    //     handler: 'index.main',
+    //     timeout: cdk.Duration.seconds(300),
+    //     runtime: lambda.Runtime.PYTHON_3_6,
+    //   })),
+    //   properties: props
+    // });
+
+    // this.response = resource.getAtt('Response').toString();
+
+    const vpc = ec2.Vpc.fromLookup(this, "lambda-VPC", {
+      vpcId: "[VPC ID]"
+    });
+    const subnets = [ec2.Subnet.fromSubnetId(this, "lambda-subnet", "[Private subnet ID]")];
+    const securityGroups = [ec2.SecurityGroup.fromSecurityGroupId(this, "inter-vpc-sg", "[Security group ID]")];
+    
+    const onEvent = new PythonFunction(this, 'MyHandler', {
+      entry: 'lib/',
+      index: 'custom-resource-handler.py',
+      handler: 'main',
+      runtime: lambda.Runtime.PYTHON_3_8,
+      vpc: vpc,
+      vpcSubnets: {subnets: subnets},
+      securityGroups: securityGroups,
+      allowPublicSubnet: true // Can set this value on a regular lambda!
+    })
+    
+    const myRole = new iam.Role(this, 'MyRole', { 
+      roleName: 'providerRole',
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaVPCAccessExecutionRole")
+      ]
+    });
+
+    const myProvider = new cr.Provider(this, 'MyProvider', {
+      onEventHandler: onEvent,
+      logRetention: logs.RetentionDays.ONE_DAY,   // default is INFINITE
+      role: myRole, // must be assumable by the `lambda.amazonaws.com` service principal
+      vpc: vpc,
+      vpcSubnets: {subnets: subnets},
+      securityGroups: securityGroups,
+      // allowPublicSubnet: true // Can't set this value on the provider lambda!
+    });
+
+    new cdk.CustomResource(this, 'Resource1', { serviceToken: myProvider.serviceToken });
   }
 }
